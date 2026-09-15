84259ae00d92889e966ee643e53e577b11068ae8
Yazid Bokhari <yabokhar@z3r9p5.42lyon.fr>
2026-09-15 20:23:39 +0200

    Fix: put back the to_remove fd fetching

--- sources/broadcast.c
+++ sources/broadcast.c
@@ -8,8 +8,11 @@ void	broadcast(t_server *server, const int except)
 	t_client		*client;
 	const size_t	message_length = strlen(server->log_buffer);
 	t_client_error	error;
+	int				to_remove[FD_SETSIZE];
+	int				remove_count;
 
 	client = server->head;
+	remove_count = 0;
 	while (client)
 	{
 		if (client->fd ^ except)
@@ -18,8 +21,10 @@ void	broadcast(t_server *server, const int except)
 			if (error == CLIENT_ERR_NONE)
 				FD_SET(client->fd, &server->active_write);
 			else if (error == CLIENT_ERR_EXCEEDS_MAX_LENGTH)
-				remove_client(server, client->fd);
+				to_remove[remove_count++] = client->fd;
 		}
 		client = client->next;
 	}
+	for (int i = 0; i < remove_count; ++i)
+		remove_client(server, to_remove[i]);
 }
